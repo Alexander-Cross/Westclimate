5da0c620d8fb97efb0e124509ae83537607248ef
Alexander_Cross <sendtocross@ya.ru>
2023-05-20 11:33:58 +0300

    catalog page is done

--- js/main_page.js
+++ js/main_page.js
@@ -353,7 +353,16 @@ window.addEventListener('load', () => {
         }
     });
 
-
+    // map
+    ymaps.ready(init);
+    function init(){
+        let myMap = new ymaps.Map("map", {
+            center: [56.15911345141619,40.38851895767211],
+            zoom: 17
+        });
+        let myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {});
+        myMap.geoObjects.add(myPlacemark);
+    }
 
 
 });
